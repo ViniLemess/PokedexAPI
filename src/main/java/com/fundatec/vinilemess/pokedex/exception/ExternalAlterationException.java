2c535fa30c7b16408d43884327508f9da163c268
ViniLemess <viniciuslemescaetano@gmail.com>
2022-08-07 20:42:30 -0300

    Exception for external api alterations

--- src/main/java/com/fundatec/vinilemess/pokedex/exception/ExternalAlterationException.java
+++ src/main/java/com/fundatec/vinilemess/pokedex/exception/ExternalAlterationException.java
@@ -1,7 +1,7 @@
 package com.fundatec.vinilemess.pokedex.exception;
 
 public class ExternalAlterationException extends RuntimeException {
-    public ExternalAlterationException() {
-        super("Cannot delete external pokemon!");
+    public ExternalAlterationException(String message) {
+        super(message);
     }
 }
