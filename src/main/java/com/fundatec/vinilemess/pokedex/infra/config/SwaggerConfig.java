0745f7d0a6d5221b1f6f3ad059687fe25bca8210
ViniLemess <viniciuslemescaetano@gmail.com>
2022-08-09 16:52:04 -0300

    update swagger configuration

--- src/main/java/com/fundatec/vinilemess/pokedex/infra/config/SwaggerConfig.java
+++ src/main/java/com/fundatec/vinilemess/pokedex/infra/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -20,6 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fundatec.vinilemess.pokedex"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
     }
