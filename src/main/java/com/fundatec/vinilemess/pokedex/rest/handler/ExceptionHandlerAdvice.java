4bc40d02b0b9cecb32fa7e4292ac601e7827a32c
ViniLemess <viniciuslemescaetano@gmail.com>
2022-08-09 16:53:22 -0300

    Add new exception handlers

--- src/main/java/com/fundatec/vinilemess/pokedex/rest/handler/ExceptionHandlerAdvice.java
+++ src/main/java/com/fundatec/vinilemess/pokedex/rest/handler/ExceptionHandlerAdvice.java
@@ -1,6 +1,7 @@
 package com.fundatec.vinilemess.pokedex.rest.handler;
 
 import com.fundatec.vinilemess.pokedex.exception.DuplicatedPokemonException;
+import com.fundatec.vinilemess.pokedex.exception.ExternalAlterationException;
 import com.fundatec.vinilemess.pokedex.exception.PokemonNotFoundException;
 import com.fundatec.vinilemess.pokedex.rest.handler.dto.ConstraintViolationError;
 import com.fundatec.vinilemess.pokedex.rest.handler.dto.ConstraintViolationsDTO;
@@ -12,8 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -39,24 +42,42 @@ public class ExceptionHandlerAdvice implements ResponseErrorHandler {
     }
 
     @ExceptionHandler(PokemonNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorDTO> notFoundClientHandler(PokemonNotFoundException exception)  {
         logger.error(exception.getMessage());
         return new ResponseEntity<>(buildError(exception.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorDTO> unknownErrorHandler(Throwable exception) {
         logger.error(exception.getMessage(), exception);
         return new ResponseEntity<>(buildError("Something went wrong!"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(DuplicatedPokemonException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<ErrorDTO> duplicatedPokemonHandler(DuplicatedPokemonException exception) {
         logger.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(buildError(exception.getMessage()));
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorDTO> methodArgumentMismatchHandler(MethodArgumentTypeMismatchException exception) {
+        logger.error(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(buildError("Invalid path parameter: " + exception.getValue()));
+    }
+
+    @ExceptionHandler(ExternalAlterationException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<ErrorDTO> externalAlterationHandler(ExternalAlterationException exception) {
+        logger.error(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(buildError(exception.getMessage()));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ConstraintViolationsDTO> methodArgumentNotValidHandler(MethodArgumentNotValidException exception) {
         logger.error(exception.getMessage());
         var constraintViolationsDTO = new ConstraintViolationsDTO(exception
--- src/main/java/com/fundatec/vinilemess/pokedex/rest/handler/dto/ConstraintViolationsDTO.java
+++ src/main/java/com/fundatec/vinilemess/pokedex/rest/handler/dto/ConstraintViolationsDTO.java
@@ -3,22 +3,17 @@ package com.fundatec.vinilemess.pokedex.rest.handler.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ConstraintViolationsDTO {
 
-    private List<ConstraintViolationError> errors = new ArrayList<>();
+    private List<ConstraintViolationError> errors;
 
     private LocalDateTime timestamp;
 
     public ConstraintViolationsDTO(List<ConstraintViolationError> errors) {
         this.errors = errors;
         this.timestamp = LocalDateTime.now();
-    }
-
-    public void addError(String field, String message) {
-        errors.add(new ConstraintViolationError(field, message));
     }
 
     public List<ConstraintViolationError> getErrors() {
